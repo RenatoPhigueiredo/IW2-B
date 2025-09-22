6585e8eb5fec8427b4ac3bcd0fa436db6ef3ddec
RenatoPhigueiredo <renatofigueiredo575@gmail.com>
2025-09-21 22:28:02 -0300

    trabalho incompleto termine

--- a.php
+++ a.php
@@ -1,31 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $nome=$_POST["nome"];
-        $endereco=$_POST["endereco"];
-        $ativo=$_POST["ativo"];
+<?php
+ header("Content-Type: application/json; charset=utf-8"); //modificar o php para o tipo JSON
+ header("Access-Control-Allow-Origin: *");
+ header("access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-        $con=mysqli_connect("localhost","root","","servidor") or die("Error!");
+ //configuracao do banco de dados
+ $host="localhost";
+ $usuario="root";
+ $senha="";
+ $db="servidor";
+//ligar com o banco de dados
+ $con=new mysqli($host,$usuario,$senha,$db);
 
-        $in="INSERT into  usuarios VALUES ('$nome',
-                                        '$endereco',
-                                        '$ativo')";
+ //verificar se contem erros
+ if($con ->connect_error){
+    http_response_code(500);
+    echo json_encode(["error" => "falha de conexao: " . $con->connect_error]);
+    exit;
+ }
+ $method= $_SERVER['REQUEST_METHOD'];
 
-        $incluir=mysqli_query($con,$in);
-        if($incluir ==1){
-            echo"td certo";
+ switch($method){
+    case "GET":
+        if(isset($_GET['pesquisa'])){
+            $pesquisa = "%" . $_GET['pesquisa'] . "%";
+                //preparao caminho sql
+            $stmt= $con ->prepare("SELECT * FROM usuarios WHERE NOME LIKE ?");
+                //evita sql injection 
+            $stmt ->bind_param("s",$pesquisa);
+                //executa a query
+            $stmt ->execute();
+                //salva o retorno da pesquisa no banco de dados para variavel result
+            $result = $stmt->get_result();
         }
         else{
-            echo"td errado";
+                //busca uma lista de usuarios ordenadas pelo ID  chave primaria
+            $result=$con->query("SELECT * FROM usuarios order by ID desc");
         }
+        $retorno = [];
 
-    ?>
-</body>
-</html>
+        while ($linha = $result->fetch_assoc()){
+            $retorno[]= $linha;
+        }
+        echo json_encode($retorno);
+        break;
+    
+    case "POST":
+        //le todo o conteudo do body e transforma o em json
+        $data= json_decode(file_get_contents("php://input"),true);
+        
+        $stmt=$con->prepare("INSERT INTO usuarios (NOME,GMAIL,SENHA,ATIVO) VALUES(?,?,?,?)");
+
+        $stmt->bind_param("sssi",$data['NOME'],$data['GMAIL'],$data['SENHA'],$data['ATIVO']);
+
+        $stmt->execute();
+
+        echo json_encode(["status" => "ok", "insert_id" =>$stmt->insert_id]);
+        break;
+    
+    case "PUT":
+        $data= json_decode(file_get_contents("php://input"),true);
+
+        $stmt=$con->prepare("UPDATE usuarios SET NOME=?,GMAIL=?,SENHA=?,ATIVO=? WHERE ID=?");
+
+        $stmt->bind_param("sssii",$data["NOME"],$data["GMAIL"],$data["SENHA"],$data["ATIVO"],$data["ID"]);
+        
+        $stmt->execute();
+
+        echo json_encode(["status" => "ok"]);
+        
+        break;
+    
+    case "DELETE":
+        $id=$_GET["id"];
+
+        $stmt=$con->prepare("DELETE FROM usuarios WHERE ID=?");
+
+        $stmt ->bind_param("i",$id);
+
+        $stmt->execute();
+
+        echo json_encode(["status" => "ok"]);
+        break;
+    }
+    
+    $con->close();
+?>
